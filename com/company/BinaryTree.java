de8076f5ffe7dbf2cd42e311aa7cc64779accfea
Farrukh <farrukh.sharipov99@mail.ru>
2018-03-11 16:28:43 +0300

    1

--- com/company/BinaryTree.java
+++ com/company/BinaryTree.java
@@ -1,63 +1,59 @@
 package com.company;
 
 public class BinaryTree {
-    private Node root = new Node();
-    private class Node {
+    public Node root = new Node();
+    public class Node {
         int value;
         Node left;
         Node right;
     }
+    public Node creatNewNode(int k) {
+        Node a = new Node();
+        a.value = k;
+        a.left = new Node();
+        a.right = new Node();
+        return a;
+    }
+    public Node addElement(int val){
+        Node node = root;
+        if (node == null) return  creatNewNode(val);
+        return node;
+    }
+
     boolean searchElement(int key) {
         Node element = root;
-        if (element.value == 0) element.value = key;
+        element.left = new Node();
+        element.right = new Node();
         while (element != null) {
             if (element.value == key) return true;
-            else if(element.value < key) element = element.left;
-            else if(element.value > key) element = element.right;
+            else if (element.value < key) element = element.left;
+            else if (element.value > key) element = element.right;
         }
         return false;
     }
-    public int leftElement(int key){
+
+    public boolean leftElement(int key) {
         Node element = root;
         element.left = new Node();
         element.right = new Node();
-        while (element != null){
-            if (element.value == key) return element.left.value;
-            else if(element.value < key) element = element.left;
-            else if(element.value > key) element = element.right;
+        while (element != null) {
+            if (element.value == key) return true;
+            else if (element.value < key) element = element.left;
+            else if (element.value > key) element = element.right;
         }
-        return element.value;
+        return false;
     }
-    int rightElement(int key){
+
+    boolean rightElement(int key) {
         Node element = root;
         element.left = new Node();
         element.right = new Node();
-        while (element != null){
-            if (element.value == key) return element.right.value;
-            else if(element.value < key) element = element.left;
-            else if(element.value > key) element = element.right;
+        while (element != null) {
+            if (element.value == key) return true;
+            else if (element.value < key) element = element.left;
+            else if (element.value > key) element = element.right;
         }
-        return element.value;
-    }
-    Node addElement(int key){
-        Node element = root;
-        element.left = new Node();
-        element.right = new Node();
-        while (element.value != 0){
-            if (element.value > key && element.left == null) element.right.value = key;
-            if (element.value < key && element.right == null) element.left.value = key;
-            if (key > element.value) {
-                element = element.right;
-                if (element.value > key && element.left == null) element.right.value = key;
-                if (element.value < key && element.right == null) element.left.value = key;
-            }
-            else if (key < element.value) {
-                element = element.left;
-                if (element.value > key && element.left == null) element.right.value = key;
-                if (element.value < key && element.right == null) element.left.value = key;
-            }
-        }
-        return element;
+        return false;
     }
     public boolean deleteElement(int key) {
         Node element = root;
